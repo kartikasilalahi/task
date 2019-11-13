80abe96696bc715cdc27ba2a442a444a3b11fe42
Kartika Nirwana Silalahi <tikasilalahi8@gmail.com>
2019-11-13 10:18:25 +0700

    asdfds

--- get-domain.js
+++ get-domain.js
@@ -1,6 +1,6 @@
-const getDomain=(str)=>{
-    var str = str.split('.')
-    return str[1]
+const getDomain=(input)=>{
+    var arrbaru=input.split('/')[2].split('.')
+    return arrbaru.length>2?arrbaru[1]:arrbaru[0]
 }
 console.log(getDomain('https://www.w3schools.com/')) // w3schools
 console.log(getDomain('https://open.spotify.com/artist/3SgHzT552wy2W8pNLaLk24')) // spotify
